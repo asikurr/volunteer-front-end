1bee5582d0ecf7fdfdd33047a6f014a97e9b8c4d
Asikur <asikurrahman7@gmail.com>
2020-10-05 22:22:26 +0600

    react base volunteer website front-end part.

---
+++ src/components/Login/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1S6trEGXdw6pg5tQLms_hXcuhftHJvvI",
+    authDomain: "burj-al-ara.firebaseapp.com",
+    databaseURL: "https://burj-al-ara.firebaseio.com",
+    projectId: "burj-al-ara",
+    storageBucket: "burj-al-ara.appspot.com",
+    messagingSenderId: "683916971579",
+    appId: "1:683916971579:web:d62d4acfe624de60d5961e",
+    measurementId: "G-DR46T4389X"
+};
+
+export default firebaseConfig;
